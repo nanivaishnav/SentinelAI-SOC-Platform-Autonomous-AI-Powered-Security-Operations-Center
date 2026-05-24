c6a45e2aa297af90bff95defd7d9f822ff6cdb88
Nani Vaishnav <nanivaishnav45@gmail.com>
2026-05-24 12:38:31 +0530

    from sqlalchemy imported column, integer, string, datetime and Json and gave type  and column as Json

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,24 @@
-from fastapi import FastAPI
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import DateTime
+from sqlalchemy import JSON
 
-app = FastAPI(
-    title="SentinelAI SOC Platform",
-    version="1.0.0"
-)
+from datetime import datetime
 
-@app.get("/")
-async def root():
-    return {
-        "message": "SOC AI Platform Running"
-    }
+from app.core.database import Base
+
+
+class SecurityLog(Base):
+
+    __tablename__ = "security_logs"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    source = Column(String)
+    event_type = Column(String)
+    severity = Column(String, default="low")
+
+    raw_log = Column(JSON)
+
+    created_at = Column(DateTime, default=datetime.utcnow)
